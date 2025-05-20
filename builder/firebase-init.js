0037be4c527287e3570ff9685c813e1264e2c67a
hoony8355 <84762407+hoony8355@users.noreply.github.com>
2025-05-20 18:17:46 +0900

    Create firebase-init.js

---
+++ builder/firebase-init.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import {
+  getDatabase, ref, push, get, query, orderByChild, limitToFirst
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBle_FLyJxn7v9AMQXlCo7U7hjcx88WrlU",
+  authDomain: "knight-tour-ranking.firebaseapp.com",
+  databaseURL: "https://knight-tour-ranking-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "knight-tour-ranking",
+  storageBucket: "knight-tour-ranking.appspot.com",
+  messagingSenderId: "1073626351852",
+  appId: "1:1073626351852:web:41ae6cb7db759beb703dc9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// ✅ 래핑하여 경로만으로 쉽게 참조할 수 있도록 설정
+window.db = db;
+window.dbRef = (path) => ref(db, path);
+window.dbPush = push;
+window.dbGet = get;
+window.dbQuery = query;
+window.dbOrderByChild = orderByChild;
+window.dbLimitToFirst = limitToFirst;
